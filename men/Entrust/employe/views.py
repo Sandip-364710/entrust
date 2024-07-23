8a9f0d6e2c44e071940b7b99ecd3f7de8b63409b
skpatel <skpatel@gmail.com>
2024-07-23 17:01:05 +0530

    change commit

--- men/Entrust/employe/views.py
+++ men/Entrust/employe/views.py
@@ -99,7 +99,7 @@ class SignupView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     def form_valid(self, form):
-         messages.success(self.request, 'Thanks for Signup ')  # Add success message
+         messages.success(self.request, 'Thanks for Signup')  # Add success message
          return super().form_valid(form)
 
 
